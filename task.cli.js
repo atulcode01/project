5ce70c83bd21395c0e7c450121c8cd23b16c0add
atulcode01 <kumaratul10011@gmail.com>
2026-03-12 10:21:37 +0530

    CLI Task Tracker

---
+++ task.cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const { log } = require("console");
+const fs = require("fs");
+
+const data = fs.readFileSync("task.json", "utf-8");
+const task = JSON.parse(data);
+
+const commandOne = process.argv[2]
+const taskDescription = process.argv[3]
+
+// console.log(command);
+
+if(commandOne === "add"){
+    const newTask = {
+        id: task.length + 1,
+        description: taskDescription,
+        status: "todo"
+    };
+    task.push(newTask);
+    fs.writeFileSync("task.json",JSON.stringify(task,null,2));
+    console.log("task added succesfully...");
+}
+
+if(commandOne === "list"){
+    task.forEach(task => {
+        console.log(`${task.id}. ${task.description} - ${task.status}`);
+        
+    });
+    
+
+}
