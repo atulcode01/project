523627d8eb46d40a898dd158d80bfb5b59f6bf0a
atulcode01 <kumaratul10011@gmail.com>
2026-03-12 10:51:34 +0530

    delete task updated

--- task.cli.js
+++ task.cli.js
@@ -24,8 +24,14 @@ if(commandOne === "add"){
 if(commandOne === "list"){
     task.forEach(task => {
         console.log(`${task.id}. ${task.description} - ${task.status}`);
-        
     });
-    
-
 }
+
+if(commandOne === "delete"){
+    const id = parseInt(process.argv[3]);
+    const updatedTask = task.filter(task => task.id!==id)
+    fs.writeFileSync("task.json", JSON.stringify(updatedTask, null, 2))
+    console.log("task Deleted...");
+}
+
+
