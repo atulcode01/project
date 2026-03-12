10e7b6ae1ca8e0adf5ed0d0a4d93dbf9542c125d
atulcode01 <kumaratul10011@gmail.com>
2026-03-13 01:40:05 +0530

    updated delete logic

--- task.cli.js
+++ task.cli.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const data = fs.readFileSync("task.json", "utf-8");
 const task = JSON.parse(data);
 
+//task add "first task"
 const commandOne = process.argv[2]
 const taskDescription = process.argv[3]
 
@@ -30,28 +31,37 @@ if(commandOne === "list"){
 if(commandOne === "delete"){
     const id = parseInt(process.argv[3]);
     const updatedTask = task.filter(task => task.id!==id)
-    fs.writeFileSync("task.json", JSON.stringify(updatedTask, null, 2))
-    console.log("task Deleted...");
+    const findTask = task.find(t => t.id === id)
+    if(process.argv[3] === ""){
+        console.log("Task not found...");
+    }if(!findTask){
+        console.log("Task not found...");
+    }
+    else{
+        fs.writeFileSync("task.json", JSON.stringify(updatedTask, null, 2))
+        console.log("task Deleted...");
+    }
 }
 
 if(commandOne === "done"){
     const id = parseInt(process.argv[3])
     const tasks = task.find(t => t.id === id)
-    if(task){
-        fs.writeFileSync("task.json", JSON.stringify(tasks, null, 2))
-        task.status = "Completed"
+    if(tasks){
+        tasks.status = "Completed"
+        fs.writeFileSync("task.json", JSON.stringify(task, null, 2))
     }else{
         console.log("task not found");
         
     }
     console.log("Status changed Successfully...");
 }
+
 if(commandOne === "continue"){
     const id = parseInt(process.argv[3])
     const tasks = task.find(t => t.id === id)
-    if(task){
-        fs.writeFileSync("task.json", JSON.stringify(tasks, null, 2))
-        task.status = "In Progress"
+    if(tasks){
+        tasks.status = "In Progress"
+        fs.writeFileSync("task.json", JSON.stringify(task, null, 2))
     }else{
         console.log("task not found");
         
